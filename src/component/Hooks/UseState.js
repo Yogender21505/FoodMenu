dd1c344a9efb53e6019a7ff8561f829230da58bb
Yogender21505 <yogender21505@iiitd.ac.in>
2022-09-02 14:02:30 +0530

    ThirdProject

---
+++ src/component/Hooks/UseState.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import "./style.css";
+
+const UseState = () => {
+
+  return (
+    <>
+    <div className='center_div'>
+        <p>my data</p>
+        <div class="button2">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Ince
+        </div>
+        <div class="button2" >
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        Dicr
+        </div>
+    </div>
+    </>
+  );
+};
+
+export default UseState;
